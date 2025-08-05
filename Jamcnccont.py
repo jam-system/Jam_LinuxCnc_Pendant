9856e63d36dc4ec4117d97f39b3de9ca9e213972
bert meelberg <bert@meelberg.net>
2025-08-06 07:20:31 +1000

    update

--- Jamcnccont.py
+++ Jamcnccont.py
@@ -105,10 +105,75 @@ class SerialFramer:
     def _contains_control_chars(self, s: str) -> bool:
         return any(ord(ch) < 0x20 and ch not in '\n\r' for ch in s)
 
+# #########################################################
 
+# #########################################################
+# Called every pollRate seconds to compare the current linuxcnc
+# state with sent state and write changes to the serial port
+# #TODO Investigate https://www.linuxcnc.org/docs/html/gui/GStat.html
+def poll(self):
+    # update linuxcnc state
+    self.ls.poll()
+
+    # Compare and send
+    if (self.task_state != self.ls.task_state):
+      #LOG.debug('task state changed", self.ls.task_state)
+      # linuxcnc.STATE_ESTOP: 1, STATE_ESTOP_RESET: 2, STATE_ON: 4
+      # STATE_OFF: 3? Appears to not be used
+      self.resetState()
+
+    # Skip anything that is not required when machine is not on
+    #if ( self.ls.task_state == self.linuxcnc.STATE_ON ):
+# #########################################################
 
 class Jamcnccont:
-  
+     # Used to feed back running, paused or stopped state for auto 
+    PROGRAM_STATE_NONE=0
+    PROGRAM_STATE_RUNNING=1
+    PROGRAM_STATE_PAUSED=2
+    PROGRAM_STATE_STOPPED=3
+
+    # Heartbeat timeout in seconds
+    HEARTBEAT_MAX=10
+
+    linuxcnc = None
+    hal = None #hal
+    mmc = None #Component
+    ls = None #linuxcnc stat
+    lc = None # command
+    le = None # error
+    #gstat = None
+    inifile = None
+    error = None
+    
+    running = False
+
+    last_heartbeat = 0
+
+    task_state = None
+    interp_state = None
+    interpreter_errcode = None
+    #actual_position = [None, None, None, None, None, None, None, None]
+    #max_velocity = 0
+    #velocity = 0
+    motion_type = None # 1, 2, or 3 (so far)
+    #command = None
+    #axis_velocity = [None, None, None, None, None, None, None, None]
+    current_vel = 0
+    # We hold jogVelocity as uom per minute as this is what is diplayed 
+    # LinuxCNC Python interface requires uom per second 
+    jog_velocity = 1000
+    paused = None
+    task_paused = None
+    file = None
+    state = None
+    program_state = None
+
+    #Used to map 0-8 axis id to axis name for MDI
+    axes = 0 #Number of configured axes
+    axesMap = 'XYZABCUVW'
+
+    
     def __init__(self, _linuxcnc, _hal, _mmc, _framer):
         self.linuxcnc = _linuxcnc
         self.ls = self.linuxcnc.stat()
@@ -116,6 +181,27 @@ class Jamcnccont:
         self.hal = _hal
         self.mmc = _mmc
         self.serialF = _framer('/dev/ttyUSB0', baudrate=115200)
+
+    # #########################################################
+    # Called every pollRate seconds to compare the current linuxcnc
+    # state with sent state and write changes to the serial port
+    # #TODO Investigate https://www.linuxcnc.org/docs/html/gui/GStat.html
+    def poll(self):
+        # update linuxcnc state
+        self.ls.poll()
+    
+    
+        # Compare and send
+        if (self.task_state != self.ls.task_state):
+            LOG.debug("task state changed", self.ls.task_state)
+            # linuxcnc.STATE_ESTOP: 1, STATE_ESTOP_RESET: 2, STATE_ON: 4
+            # STATE_OFF: 3? Appears to not be used
+            self.resetState()
+
+
+
+
+
 
     # #########################################################
     # Run the loop to check incoming serial data and
@@ -130,27 +216,9 @@ class Jamcnccont:
                 print(f"Received Command: {self.msg.command}, "
                         f"Parameter: {self.msg.parameter}, "
                         f"Data: {self.msg.data}")
+            self.poll   
             else:
                 self.ls.poll()
-                x,y,z,a,b,c,u,v,w = self.ls.actual_position
-                # NOTE: Ignore ABCUVW since not used for my machine
-                # Subtract G5x offset
-                xo,yo,zo,ao,bo,co,uo,vo,wo = self.ls.g5x_offset
-                ##print(f"{xo:.3f},{yo:.3f},{zo:.3f}")
-                x -= xo
-                y -= yo
-                z -= zo
-
-                print(f"bbb {x:.3f},{y:.3f},{z:.3f}")
-                # Subtract tool offset
-                xo,yo,zo,ao,bo,co,uo,vo,wo = self.ls.tool_offset
-               ## print(f"Tool offset: {xo},{yo},{zo}")
-                x -= xo
-                y -= yo
-                z -= zo
-
-                # Print offset coordinates
-                print(f"aaa {x:.3f},{y:.3f},{z:.3f}")
                 time.sleep(0.24)
 
     def isRunning(self):
